5f3f80745d9fa0181f0f0dca32131e67055af7af
Ivo Dias <igd753@gmail.com>
2021-01-06 15:23:40 -0300

    Create PipelineScan

---
+++ PipelineScan.ps1
@@ -0,0 +1,48 @@
+# PipelineScan
+
+# Funcao para validar os arquivos
+function PipeScan {
+    param (
+        $arquivo,
+        $veracodeID,
+        $veracodeAPIkey
+
+    )
+    try {
+        # Filtra os objetos
+        $nomearquivo = $arquivo.name
+        $caminhoarquivo = $arquivo.fullname
+        # Scan
+        java -jar "./PipeScan/pipeline-scan.jar" -f $caminhoarquivo -vid $veracodeID -vkey $veracodeAPIkey
+    }
+    catch {
+        $ErrorMessage = $_.Exception.Message # Recebe o erro
+        Write-Host "Erro ao verificar o arquivo: $nomearquivo"
+        Write-Host "$ErrorMessage"
+    }
+}
+
+# Configuracoes
+$urlPipeScan = "https://downloads.veracode.com/securityscan/pipeline-scan-LATEST.zip"
+$veracodeID = ""
+$veracodeAPIkey = ""
+$tipofiltro = "jar"
+
+try {
+    # Faz o download
+    Invoke-WebRequest -Uri "$urlPipeScan" -OutFile "./pipescan.zip"
+    # Extrai o arquivo
+    Expand-Archive -Path "./pipescan.zip" -DestinationPath "./PipeScan"
+}
+catch {
+    $ErrorMessage = $_.Exception.Message # Recebe o erro
+    Write-Host "Erro ao fazer o download do Pipeline Scan"
+    Write-Host "$ErrorMessage"
+}
+
+# Filtra os arquivos
+$arquivos = Get-ChildItem "./*" -Include "*.$tipofiltro" -recurse
+# Faz a verificacao
+foreach ($arquivo in $arquivos) {
+    PipeScan $arquivo $veracodeID $veracodeAPIkey
+}
