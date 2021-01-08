cb961d1e1e39d2869bcc3de13c7a91896af44b5c
Ivo Dias <igd753@gmail.com>
2021-01-08 10:50:09 -0300

    Pipe Scan para Linux

---
+++ PipelineScan.sh
@@ -0,0 +1,12 @@
+# Configuracoes
+urlPipeScan="https://downloads.veracode.com/securityscan/pipeline-scan-LATEST.zip"
+veracodeID=""
+veracodeAPIkey=""
+caminhoArquivo="./teste.zip"
+
+# Faz o download da ferramenta
+curl -o pipescan.zip $urlPipeScan
+# Descompacta a ferramenta
+unzip -o pipescan.zip -d "./"
+# Faz a analise
+java -jar "pipeline-scan.jar" -f "*$caminhoArquivo" -vid $veracodeID -vkey $veracodeAPIkey
